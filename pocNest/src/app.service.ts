867456618ba62d9dbbe8f03b88dc9787d8a96c03
Sonny-Fourmont <sonny.fourmont@epitech.eu>
2024-09-17 17:45:13 +0200

    [ADD] cron job

--- pocNest/src/app.service.ts
+++ pocNest/src/app.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name)
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  handleCron() {
+    this.logger.debug('called every minutes')
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
