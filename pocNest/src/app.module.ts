ec85f9e58d36c5051a8e2a7220ad0fbb2506c8a0
Sonny-Fourmont <sonny.fourmont@epitech.eu>
2024-09-24 17:46:41 +0200

    [ADD] google login system with cron job for send email

--- pocNest/src/app.module.ts
+++ pocNest/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { GmailService } from './gmail/gmail.service';
+import { GmailController } from './gmail/gmail.controller';
 
 @Module({
   imports: [
-    ScheduleModule.forRoot()
+    ScheduleModule.forRoot(),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, GmailController],
+  providers: [AppService, GmailService],
 })
 export class AppModule {}
