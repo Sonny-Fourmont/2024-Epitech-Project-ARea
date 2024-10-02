1ca3bc166e1421f10aa7380670763a91fd7168ba
Sonny-Fourmont <sonny.fourmont@epitech.eu>
2024-10-02 10:56:37 +0200

    [ADD] First implementation of the password hashing system

---
+++ backend/utils/bcrypt.go
@@ -0,0 +1,13 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func GenerateHash(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 15)
+	return string(hash), err
+}
+
+func CheckHashPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
