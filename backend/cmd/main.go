ac7a059b6e2330067a42bd86fc7e056e854f8c48
Sonny-Fourmont <sonny.fourmont@epitech.eu>
2024-10-21 14:52:52 +0200

    [HOTFIX] add cors for conntection between front and back

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"area/config"
 	"area/routes"
 	"area/services"
 	storage "area/storage"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,6 +18,14 @@ func main() {
 	storage.ConnectDatabase()
 
 	router := gin.Default()
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowAllOrigins = true
+	corsConfig.AllowMethods = []string{"POST", "GET", "PUT", "DELETE"}
+	corsConfig.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent"}
+	corsConfig.AllowCredentials = true
+	corsConfig.MaxAge = 12 * time.Hour
+
+	router.Use(cors.New(corsConfig))
 
 	routes.InitRoutes(router)
 
