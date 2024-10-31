4d053bf5142fe3f214d82ffd3e294f4bb76aa2c2
Matribuk <antonin.leprest@epitech.eu>
2024-10-31 15:39:44 +0100

    [ADD] : Service spotify ready
    Signed-off-by: Matribuk <antonin.leprest@epitech.eu>

--- backend/services/service.go
+++ backend/services/service.go
@@ -5,8 +5,8 @@ type ActionFunc func(userID string, this string) []string
 var actions = map[string]ActionFunc{
 	//"youtube_latest_video": LatestVideoAction,
 	//"youtube_liked_video":  LikedVideoAction,
-	"google_latest_mail":   LatestMailAction,
-	"spotify_latest_song":  LatestSongAction,
+	"google_latest_mail": LatestMailAction,
+	//"spotify_latest_song":  LatestSongAction,
 }
 
 type ReActionFunc func(userID string, that string, dataAction []string)
