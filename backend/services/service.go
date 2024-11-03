babb389e6c97bee425b1ef11534f1289e9468f70
Matribuk <antonin.leprest@epitech.eu>
2024-11-03 23:35:35 +0100

    [HOTFIX] cronJob enable
    Signed-off-by: Matribuk <antonin.leprest@epitech.eu>

--- backend/services/service.go
+++ backend/services/service.go
@@ -3,10 +3,10 @@ package services
 type ActionFunc func(userID string, this string) []string
 
 var actions = map[string]ActionFunc{
-	//"youtube_latest_video": LatestVideoAction,
-	//"youtube_liked_video":  LikedVideoAction,
-	//"google_latest_mail": LatestMailAction,
-	//"spotify_latest_song":  LatestSongAction,
+	"youtube_latest_video": LatestVideoAction,
+	"youtube_liked_video":  LikedVideoAction,
+	"google_latest_mail":   LatestMailAction,
+	"spotify_latest_song":  LatestSongAction,
 }
 
 type ReActionFunc func(userID string, that string, dataAction []string)
