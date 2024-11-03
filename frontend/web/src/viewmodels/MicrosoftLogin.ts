9fccdb84af75a628e962ccad2ec2142753ee1436
RainproDash <vivien.boitard@epitech.eu>
2024-11-03 13:32:35 +0100

    [ADD] callback route for frontend

--- frontend/web/src/viewmodels/MicrosoftLogin.ts
+++ frontend/web/src/viewmodels/MicrosoftLogin.ts
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { BACKEND_URL } from 'config';
 
 export default function useMicrosoftLogin() {
     const handleMicrosoftLogin = () => {
+        window.location.href = `${BACKEND_URL}/microsoft/login`;
     };
 
     return (
